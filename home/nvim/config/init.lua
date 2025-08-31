9f44f9f7f7fa9823066df6f45f3c5ca48af4f8c8
blckSwan <blckswvn@proton.me>
2025-08-31 22:41:46 +0200

    minor config tweaks

--- home/nvim/config/init.lua
+++ home/nvim/config/init.lua
@@ -16,9 +16,8 @@ vim.opt.clipboard = "unnamedplus"
 -- binds
 vim.g.mapleader = " "
 
-vim.keymap.set("n", "<leader>f", ":FzfLua files<CR>")
 vim.keymap.set("n", "<leader>g", ":FzfLua grep<CR>")
-vim.keymap.set("n", "<leader>z", ":FzfLua<CR>")
+vim.keymap.set("n", "<leader>z", ":FzfLua <CR>")
 
 vim.keymap.set("n", "<leader>o", ":lua MiniFiles.open()<CR>")
 
@@ -44,7 +43,8 @@ vim.keymap.set("n", "<leader>Gp", ":Gitsigns preview_hunk<CR>")
 vim.pack.add({
 	{src = "https://github.com/nvim-treesitter/nvim-treesitter"},
 	{src = "https://github.com/windwp/nvim-autopairs"},
-	{src = "https://github.com/nyoom-engineering/oxocarbon.nvim"},
+	--{src = "https://github.com/nyoom-engineering/oxocarbon.nvim"},
+	{src = "https://github.com/ashish2508/Eezzy.nvim" },
 	{src = "https://github.com/ibhagwan/fzf-lua"},
 	{src = "https://github.com/nvim-tree/nvim-web-devicons"},
 	{src = "https://github.com/lukas-reineke/indent-blankline.nvim"},
@@ -77,23 +77,67 @@ require("mini.files").setup({
 	}
 })
 
-vim.cmd.colorscheme("oxocarbon")
+--vim.cmd.colorscheme("oxocarbon")
+vim.cmd.colorscheme("Eezzy")
 
+vim.api.nvim_set_hl(0, "FloatBorder", { fg = "#ffffff", bg = nil })
+vim.api.nvim_set_hl(0, "CmpBorder", { fg = "#ffffff", bg = nil })
+
+local border = "rounded"
+
+-- Override handlers to use borders
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+  vim.lsp.handlers.hover,
+  { border = border }
+)
+
+vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
+  vim.lsp.handlers.signature_help,
+  { border = border }
+)
+
+-- Diagnostics float window border
+vim.diagnostic.config({
+  float = { border = border }
+})
+
+-- Make :LspInfo border match too (if you use it)
+require("lspconfig.ui.windows").default_options.border = border
 local lspconfig = require("lspconfig")
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = false
+
+local on_attach = function(_, bufnr)
+	local opts = { buffer = bufnr }
+	vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+	vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+	vim.keymap.set("n", "gr", function()
+		require("fzf-lua").lsp_references()
+	end, opts)
+	vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+	vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+end
+
 lspconfig.clangd.setup{
 	capabilities = capabilities,
+	on_attach = on_attach,
 }
 lspconfig.nixd.setup{
 	capabilities = capabilities,
+	on_attach = on_attach,
 }
 lspconfig.lua_ls.setup{
 	capabilities = capabilities,
+	on_attach = on_attach,
 }
+
 
 local cmp = require"cmp"
 cmp.setup({
+	window = {
+		completion = cmp.config.window.bordered(border),
+		documentation = cmp.config.window.bordered(border),
+	},
 	snippet = {
 		expand = function() end
 	},
@@ -110,6 +154,8 @@ cmp.setup({
 		completeopt = 'menu,menuone,select'
 	}
 })
+
+
 
 require("gitsigns").setup()
 
