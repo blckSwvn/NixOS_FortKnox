c93ab086e8e9eaa287fcb22e91a59388aae42fff
blckSwan <blckswvn@proton.me>
2025-09-15 22:17:47 +0200

    minor tweaks like C-d zz

--- configuration.nix
+++ configuration.nix
@@ -52,6 +52,23 @@
     shell = pkgs.zsh;
   };
 
+#disable usb from waking
+systemd.services.disable-usb-wake = {
+  description = "Disable all USB wakeup devices";
+  after = [ "multi-user.target" ];
+  serviceConfig = {
+    Type = "oneshot";
+    ExecStart = ''
+      for dev in $(grep -i usb /proc/acpi/wakeup | awk '{print $1}'); do
+        echo $dev > /proc/acpi/wakeup
+      done
+    '';
+    RemainAfterExit = true;
+  };
+};
+
+boot.kernelParams = [ "mem_sleep_default=deep" ];
+
 #for laptops laptop 
   services.logind.extraConfig = ''
     handleLidSwitch=suspend
--- modules/boot.nix
+++ modules/boot.nix
@@ -34,6 +34,7 @@
         "nowatchdog"
         "loglevel=3" #only errors and beyond are logged change how needed 4:warning 5:notice 6:info 7:debug
         "amd_pstate=active" #better for newer CPUs
+        "usbcore.autosuspend=1"
     ];
 
 #hardned kernel
